937a5a3185b0c6c2bcce3a5887ef324c65600c88
Севриков Сергей Анатольевич <SevrikovSA@pktbn.org.rzd>
2020-09-03 14:39:41 +0300

    Bem

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// $(function () {
+//   // Owl Carousel
+//   var owl = $(".owl-carousel");
+//   owl.owlCarousel({
+//     items: 3,
+//     margin: 10,
+//     loop: true,
+//     nav: true,
+//   });
+// });
+$(document).ready(function() {
+  $('.menu-burger').click(function (event)) {
+    $('.menu-burger,.nav').toggleClass('active');
+});
+
+});
